8f3d260470c469ffc480d99d9dfae4ce4c0a0b4b
KristianNilsenAug22FT <114563561+KristianNilsenAug22FT@users.noreply.github.com>
2023-11-08 14:01:30 +0100

    unit testing added

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -1,16 +1,26 @@
-const auth = require('./login'); // Replace with your actual module path
+import { login } from './login';
+
+import 'jest-localstorage-mock';
+
+beforeEach(() => {
+  localStorage.clear();
+});
 
 describe('login function', () => {
-  it('should store a token in browser storage', () => {
-    // Mock the localStorage
-    const setItemMock = jest.fn();
-    global.localStorage.setItem = setItemMock;
+  it('should store a token in browser storage', async () => {
+    // Mock the fetch function to return a successful response
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      ok: true,
+      json: () => Promise.resolve({ accessToken: 'mockToken' }),
+    });
 
     // Call the login function
-    const token = auth.login('testUser', 'testPassword');
+    await login('test@example.com', 'password');
 
-    // Assertions
-    expect(token).toEqual('your_token_here'); // Adjust to your expected token
-    expect(setItemMock).toHaveBeenCalledWith('token', 'your_token_here');
+    // Ensure that the token is stored in localStorage
+    expect(localStorage.getItem('token')).toBe(JSON.stringify('mockToken'));
+
+    // Restore the original fetch function
+    global.fetch.mockRestore();
   });
 });
--- src/js/api/auth/logout.test.js
+++ src/js/api/auth/logout.test.js
@@ -1,15 +1,20 @@
-const auth = require('./logout'); // Replace with your actual module path
+import { logout } from './logout'; // Import your logout function
+
+// Import your custom remove function
+import { remove } from "../../storage/index.js";
+
+// Mock the remove function
+jest.mock("../../storage/index.js", () => ({
+  remove: jest.fn(),
+}));
 
 describe('logout function', () => {
   it('should clear the token from browser storage', () => {
-    // Mock the localStorage
-    const removeItemMock = jest.fn();
-    global.localStorage.removeItem = removeItemMock;
-
     // Call the logout function
-    auth.logout();
+    logout();
 
     // Assertions
-    expect(removeItemMock).toHaveBeenCalledWith('token');
+    expect(remove).toHaveBeenCalledWith('token');
+    expect(remove).toHaveBeenCalledWith('profile');
   });
 });
